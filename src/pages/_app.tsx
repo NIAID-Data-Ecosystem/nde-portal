9b4f056acf3b546a32701e1f12a29f30eef318f5
candicecz <czech.candice@gmail.com>
2022-04-01 11:32:47 -0400

    [FIX] font loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type {AppProps} from 'next/app';
 import {QueryClient, QueryClientProvider} from 'react-query';
-import {ThemeProvider} from 'nde-design-system';
+import {extendTheme, theme as NDEtheme, ThemeProvider} from 'nde-design-system';
+import FontFace from 'src/theme/font-face';
 
 // Creates an instance of react-query for the app.
 const queryClient = new QueryClient();
@@ -9,7 +10,8 @@ function App({Component, pageProps}: AppProps) {
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        <ThemeProvider>
+        <ThemeProvider theme={NDEtheme}>
+          <FontFace />
           <Component {...pageProps} />
         </ThemeProvider>
       </QueryClientProvider>
