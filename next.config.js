49d7a83a3fcd541bb3f7b2d98f5e933bec542b07
candicecz <czech.candice@gmail.com>
2022-04-26 07:14:47 -0400

    [UPDATE] staging config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const isProd = process.env.NODE_ENV === 'production';
-const basePath = process.env.DEST_DIR;
 
 module.exports = {
   reactStrictMode: true,
@@ -11,7 +10,7 @@ module.exports = {
     ? `http://nde-portal.s3-website-us-east-1.amazonaws.com/staging`
     : '',
   // Prefix hyperlinks with the base path.
-  basePath: isProd ? `${process.env.DEST_DIR}` : '',
+  basePath: isProd ? `/${process.env.DEST_DIR}` : '',
   // [NOTE]:For public environment variables only.
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
