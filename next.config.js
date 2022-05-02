83a81babf791abca0d5ee32f360f0e4e197fb87c
candicecz <czech.candice@gmail.com>
2022-05-02 09:36:19 -0400

    [UPDATE] staging + production relative paths

--- next.config.js
+++ next.config.js
@@ -10,8 +10,9 @@ module.exports = {
   // Prefix hyperlinks with the base path.
   basePath: isProd ? `/${process.env.BASE_PREFIX}` : '',
   // [NOTE]:For public environment variables only.
-  // env: {
-  //   NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-  //   ASSET_PREFIX: process.env.ASSET_PREFIX,
-  // },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    ASSET_PREFIX: process.env.ASSET_PREFIX,
+    BASE_PREFIX: process.env.BASE_PREFIX,
+  },
 };
