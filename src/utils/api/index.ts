f1cc59b3a27e3b9b7bfa769ce7fbec5b989860e7
candicecz <czech.candice@gmail.com>
2022-05-06 16:53:10 -0400

    [FIX] wrap api id in quotes

--- src/utils/api/index.ts
+++ src/utils/api/index.ts
@@ -11,7 +11,7 @@ export const getResourceById = async (id?: string | string[]) => {
   }
   try {
     const {data} = await axios.get(
-      `${process.env.NEXT_PUBLIC_API_URL}/query?q=_id:${id}`,
+      `${process.env.NEXT_PUBLIC_API_URL}/query?q=_id:"${id}"`,
     );
 
     const formattedData = await formatAPIResource(data.hits[0]);
