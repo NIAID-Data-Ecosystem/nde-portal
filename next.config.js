b14355a48decff3a32b328f448ccf94b18916306
candicecz <czech.candice@gmail.com>
2022-05-12 13:06:07 -0400

    [UPDATE] path

--- next.config.js
+++ next.config.js
@@ -5,11 +5,11 @@ const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
   reactStrictMode: true,
   trailingSlash: true,
-  assetPrefix: '',
-  basePath: '',
+  assetPrefix: `${process.env.BASE_URL}`,
+  basePath: `${process.env.BASE_URL}`,
 
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-    BASE_URL: isProd ? process.env.BASE_URL : '',
+    BASE_URL: process.env.BASE_URL,
   },
 };
