b2854fd33d6571efb628cb06614ac42df434cbfa
candicecz <czech.candice@gmail.com>
2022-05-12 10:07:25 -0400

    [FIX] routing

--- next.config.js
+++ next.config.js
@@ -4,10 +4,21 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   reactStrictMode: true,
-  trailingSlash: true,
+  // trailingSlash: true,
   assetPrefix: '',
   basePath: '',
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    BASE_PREFIX: '/portal',
+  },
+  exportPathMap: async function (
+    defaultPathMap,
+    {dev, dir, outDir, distDir, buildId},
+  ) {
+    return {
+      '/': {page: '/'},
+      '/portal/about': {page: '/about'},
+      '/portal/search': {page: '/search'},
+    };
   },
 };
