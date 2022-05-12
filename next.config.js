f3f63d7712f1db2124d77b86071c9e1cef816a75
candicecz <czech.candice@gmail.com>
2022-05-12 13:44:46 -0400

    [UPDATE] trailing slash

--- next.config.js
+++ next.config.js
@@ -4,12 +4,27 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   reactStrictMode: true,
-  trailingSlash: true,
+  // trailingSlash: true,
   assetPrefix: isProd ? process.env.BASE_URL : '',
   basePath: isProd ? process.env.BASE_URL : '',
 
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     BASE_URL: isProd ? process.env.BASE_URL : '',
+  },
+
+  exportPathMap: async function (
+    defaultPathMap,
+    {dev, dir, outDir, distDir, buildId},
+  ) {
+    const newPathMap = Object.entries(defaultPathMap).reduce((r, [k, page]) => {
+      if (k === '/') {
+        r[k] = page;
+      } else {
+        r[`${k}/index`] = page;
+      }
+      return r;
+    }, {});
+    return newPathMap;
   },
 };
