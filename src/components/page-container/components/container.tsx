ca2b83702c29ae3d56c6e8c9d3e056144cfbb83f
candicecz <czech.candice@gmail.com>
2022-05-18 13:44:16 -0400

    [FIX] links

--- src/components/page-container/components/container.tsx
+++ src/components/page-container/components/container.tsx
@@ -1,11 +1,11 @@
-import {useRef} from 'react';
-import {Box, Flex, Footer, FlexProps, Navigation} from 'nde-design-system';
-import navItems from 'configs/nav.json';
-import footerItems from 'configs/footer.json';
-import Head from 'next/head';
-import Notice from './notice';
-import {env} from 'next.config';
-import {SearchBar} from 'src/components/search-bar';
+import { useRef } from "react";
+import { Box, Flex, Footer, FlexProps, Navigation } from "nde-design-system";
+import navItems from "configs/nav.json";
+import footerItems from "configs/footer.json";
+import Head from "next/head";
+import Notice from "./notice";
+import { env } from "next.config";
+import { SearchBar } from "src/components/search-bar";
 
 interface PageContainerProps extends FlexProps {
   hasNavigation?: boolean;
@@ -14,7 +14,7 @@ interface PageContainerProps extends FlexProps {
   disableSearchBar?: boolean;
 }
 
-export const NAV_HEIGHT = {base: '105px', sm: '77px', md: '89px'};
+export const NAV_HEIGHT = { base: "105px", sm: "77px", md: "89px" };
 
 export const PageContainer: React.FC<PageContainerProps> = ({
   children,
@@ -27,15 +27,24 @@ export const PageContainer: React.FC<PageContainerProps> = ({
   const ref = useRef<HTMLDivElement>(null);
 
   const formatRoute: any = (routes: any[]) => {
-    return routes.map(r => {
-      if ('routes' in r) {
-        return {...r, routes: formatRoute(r.routes)};
+    return routes.map((r) => {
+      if ("routes" in r) {
+        return { ...r, routes: formatRoute(r.routes) };
       }
+
+      // if relative link, prefix with backslash
+      if (r["href"].charAt(0) === "/") {
+        return {
+          ...r,
+          href: `${env.BASE_URL}${r["href"]}${
+            r["href"].slice(-1) === "/" ? "" : "/"
+          }`,
+        };
+      }
+
       return {
         ...r,
-        href: `${env.BASE_URL}${r['href']}${
-          r['href'].slice(-1) === '/' ? '' : '/'
-        }`,
+        href: r["href"],
       };
     });
   };
@@ -44,18 +53,18 @@ export const PageContainer: React.FC<PageContainerProps> = ({
     <>
       <Head>
         <title>NDE Portal {title && ` | ${title}`}</title>
-        <meta name='description' content={metaDescription} />
+        <meta name="description" content={metaDescription} />
       </Head>
 
-      <Flex as={'main'} w={'100%'} flexDirection={'column'} minW={300}>
+      <Flex as={"main"} w={"100%"} flexDirection={"column"} minW={300}>
         {hasNavigation && (
           // Sticky Nav Bar.
           <Box
-            id='nav-wrapper'
+            id="nav-wrapper"
             ref={ref}
-            position='sticky'
+            position="sticky"
             top={0}
-            w='100%'
+            w="100%"
             zIndex={100}
             minW={300}
           >
@@ -64,7 +73,7 @@ export const PageContainer: React.FC<PageContainerProps> = ({
         )}
 
         {/*Page content has margin-top to compensate for fixed nav bar. */}
-        <Box id={'pagebody'} position='relative'>
+        <Box id={"pagebody"} position="relative">
           <Notice />
           {!disableSearchBar && <SearchBar />}
           {children}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import {useState} from 'react';
-import type {NextPage} from 'next';
+import { useState } from "react";
+import type { NextPage } from "next";
 import {
   Box,
   Button,
@@ -13,17 +13,17 @@ import {
   Text,
   useBreakpointValue,
   usePrefersReducedMotion,
-} from 'nde-design-system';
-import {PageContainer, PageContent} from 'src/components/page-container';
-import {useRouter} from 'next/router';
-import homepageCopy from 'configs/homepage.json';
-import {FaChevronRight} from 'react-icons/fa';
-import {fetchSearchResults} from 'src/utils/api';
-import {useQuery} from 'react-query';
-import {FetchSearchResultsResponse} from 'src/utils/api/types';
-import LoadingSpinner from 'src/components/loading';
-import {formatNumber} from 'src/utils/helpers';
-import PieChart from 'src/components/home/components/pie-chart';
+} from "nde-design-system";
+import { PageContainer, PageContent } from "src/components/page-container";
+import { useRouter } from "next/router";
+import homepageCopy from "configs/homepage.json";
+import { FaChevronRight } from "react-icons/fa";
+import { fetchSearchResults } from "src/utils/api";
+import { useQuery } from "react-query";
+import { FetchSearchResultsResponse } from "src/utils/api/types";
+import LoadingSpinner from "src/components/loading";
+import { formatNumber } from "src/utils/helpers";
+import PieChart from "src/components/home/components/pie-chart";
 import {
   fade,
   StyledSection,
@@ -31,13 +31,13 @@ import {
   StyledText,
   StyledBody,
   StyledSectionButtonGroup,
-} from 'src/components/home/styles';
-import {assetPrefix, env} from 'next.config';
-import NextLink from 'next/link';
+} from "src/components/home/styles";
+import { assetPrefix, env } from "next.config";
+import NextLink from "next/link";
 
 const sample_queries = [
   {
-    title: 'E. coli',
+    title: "E. coli",
     searchTerms: [
       '"E.coli"',
       '"Escherichia coli"',
@@ -45,8 +45,8 @@ const sample_queries = [
       '"STEC"',
     ],
   },
-  {title: 'Tuberculosis', searchTerms: ['"Tuberculosis"', '"TB"']},
-  {title: 'Ebola', searchTerms: ['"Ebola"', '"EBOV"', '"EVD"']},
+  { title: "Tuberculosis", searchTerms: ['"Tuberculosis"', '"TB"'] },
+  { title: "Ebola", searchTerms: ['"Ebola"', '"EBOV"', '"EVD"'] },
 ];
 
 interface QuickQueryLinkProps {
@@ -62,24 +62,27 @@ const QuickQueryLink: React.FC<QuickQueryLinkProps> = ({
     return null;
   }
   return (
-    <NextLink href={{pathname: `/search`, query: {q: queryString}}} passHref>
+    <NextLink
+      href={{ pathname: `/search`, query: { q: queryString } }}
+      passHref
+    >
       <Link
         mx={2}
-        color='whiteAlpha.800'
+        color="whiteAlpha.800"
         _hover={{
-          color: 'white',
-          textDecoration: 'underline',
-          svg: {transform: 'translateX(0)', transition: '0.2s ease-in-out'},
+          color: "white",
+          textDecoration: "underline",
+          svg: { transform: "translateX(0)", transition: "0.2s ease-in-out" },
         }}
-        _visited={{color: 'white'}}
+        _visited={{ color: "white" }}
       >
         <Text>{title}</Text>
         <Icon
           as={FaChevronRight}
           ml={2}
           boxSize={3}
-          transform='translateX(-5px)'
-          transition='0.2s ease-in-out'
+          transform="translateX(-5px)"
+          transition="0.2s ease-in-out"
         ></Icon>
       </Link>
     </NextLink>
@@ -88,10 +91,10 @@ const QuickQueryLink: React.FC<QuickQueryLinkProps> = ({
 
 const Home: NextPage = () => {
   const router = useRouter();
-  const size = useBreakpointValue({base: 200, sm: 200, lg: 200, xl: 200});
+  const size = useBreakpointValue({ base: 200, sm: 200, lg: 200, xl: 200 });
 
   // Search term entered in search bar
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void =>
     setSearchTerm(e.target.value);
 
@@ -103,13 +106,13 @@ const Home: NextPage = () => {
 
   // Fetch stats about number of resources
   const params = {
-    q: '__all__',
+    q: "__all__",
     size: 0,
     facets: [
-      '@type',
-      'measurementTechnique.name',
-      'includedInDataCatalog.name',
-    ].join(','),
+      "@type",
+      "measurementTechnique.name",
+      "includedInDataCatalog.name",
+    ].join(","),
     facet_size: 20,
   };
 
@@ -132,45 +135,45 @@ const Home: NextPage = () => {
     measurementTechnique: null,
     repositories: null,
   });
-  const {isLoading, error} = useQuery<
+  const { isLoading, error } = useQuery<
     FetchSearchResultsResponse | undefined,
     Error
   >({
-    queryKey: ['stats', params],
+    queryKey: ["stats", params],
     queryFn: () => fetchSearchResults(params),
     refetchOnWindowFocus: false,
-    onSuccess: data => {
-      let stat = {...stats};
+    onSuccess: (data) => {
+      let stat = { ...stats };
       if (data) {
-        const {facets} = data;
+        const { facets } = data;
 
         // Data types - we're interested in computationaltool and datasets.
-        const types: {[key: string]: Stat} = facets['@type'].terms.reduce(
-          (r: {[key: string]: Stat}, v: Stat) => {
+        const types: { [key: string]: Stat } = facets["@type"].terms.reduce(
+          (r: { [key: string]: Stat }, v: Stat) => {
             const key = v.term.toLowerCase();
-            if (key === 'dataset' || key === 'computationaltool') {
+            if (key === "dataset" || key === "computationaltool") {
               if (!r[`${key}`]) {
-                r[`${key}`] = {term: '', count: 0};
+                r[`${key}`] = { term: "", count: 0 };
               }
               r[`${key}`].term =
-                key === 'computationaltool' ? 'Tools' : 'Datasets';
+                key === "computationaltool" ? "Tools" : "Datasets";
               r[`${key}`].count += v.count;
             }
             return r;
           },
-          {},
+          {}
         );
         // Get number of measurement techniques
         const measurementTechnique = {
-          term: 'Measurement techniques',
-          count: facets['measurementTechnique.name'].total,
+          term: "Measurement techniques",
+          count: facets["measurementTechnique.name"].total,
         };
 
-        const sources = [...facets['includedInDataCatalog.name'].terms];
+        const sources = [...facets["includedInDataCatalog.name"].terms];
 
         // Get number of repositories
         const repositories = {
-          term: 'Repositories',
+          term: "Repositories",
           count: sources.length,
           stats: sources,
         };
@@ -189,92 +192,92 @@ const Home: NextPage = () => {
     <>
       <PageContainer
         hasNavigation
-        title='Search'
-        metaDescription='Discovery Portal home page.'
+        title="Search"
+        metaDescription="Discovery Portal home page."
         disableSearchBar
       >
         <PageContent
           bgImg={`${assetPrefix}/assets/home-bg.png`}
-          backgroundSize='cover'
-          flexWrap='wrap'
-          minH='unset'
-          justifyContent={{xl: 'center'}}
+          backgroundSize="cover"
+          flexWrap="wrap"
+          minH="unset"
+          justifyContent={{ xl: "center" }}
         >
           {/* Header section */}
           <StyledSection
-            id='header'
-            flexDirection={'column'}
-            alignItems={{base: 'flex-start', xl: 'center'}}
-            textAlign={{xl: 'center'}}
+            id="header"
+            flexDirection={"column"}
+            alignItems={{ base: "flex-start", xl: "center" }}
+            textAlign={{ xl: "center" }}
           >
-            <Box maxW='600px'>
+            <Box maxW="600px">
               <Heading
-                as='h1'
-                size='h1'
-                color='white'
-                fontWeight='bold'
+                as="h1"
+                size="h1"
+                color="white"
+                fontWeight="bold"
                 letterSpacing={1}
-                lineHeight='shorter'
+                lineHeight="shorter"
                 animation={animation}
               >
                 {homepageCopy.sections[0].heading}
               </Heading>
               <Text
-                color='white'
-                fontSize='xl'
-                fontWeight='semibold'
+                color="white"
+                fontSize="xl"
+                fontWeight="semibold"
                 mt={4}
                 animation={animation}
-                sx={{animationDelay: '1s'}}
+                sx={{ animationDelay: "1s" }}
               >
                 {homepageCopy.sections[0].subtitle}
               </Text>
               <Text
-                color='white'
-                fontWeight='light'
-                fontSize='lg'
-                lineHeight='short'
+                color="white"
+                fontWeight="light"
+                fontSize="lg"
+                lineHeight="short"
                 mt={2}
-                maxWidth={{base: '400px', xl: 'unset'}}
+                maxWidth={{ base: "400px", xl: "unset" }}
                 animation={animation}
-                sx={{animationDelay: '1.5s'}}
+                sx={{ animationDelay: "1.5s" }}
               >
                 {homepageCopy.sections[0].body}
               </Text>
             </Box>
-            <Flex w='100%' mt={[15, 20, 24]} justifyContent='center'>
+            <Flex w="100%" mt={[15, 20, 24]} justifyContent="center">
               <Flex
-                flexDirection='column'
-                maxW={{base: '600px', xl: '1000px'}}
-                w='100%'
+                flexDirection="column"
+                maxW={{ base: "600px", xl: "1000px" }}
+                w="100%"
               >
                 <SearchInput
-                  w='100%'
+                  w="100%"
                   isResponsive={false}
-                  colorScheme='primary'
-                  ariaLabel='Search for datasets or tools'
-                  placeholder='Search for datasets or tools'
+                  colorScheme="primary"
+                  ariaLabel="Search for datasets or tools"
+                  placeholder="Search for datasets or tools"
                   value={searchTerm}
                   handleChange={handleChange}
-                  handleSubmit={e => {
+                  handleSubmit={(e) => {
                     e.preventDefault();
 
                     router.push({
                       pathname: `/search`,
-                      query: {q: searchTerm.trim()},
+                      query: { q: searchTerm.trim() },
                     });
                   }}
                 />
-                <Flex mt={2} flexWrap={['wrap']}>
-                  <Text color='whiteAlpha.800' mr={2}>
+                <Flex mt={2} flexWrap={["wrap"]}>
+                  <Text color="whiteAlpha.800" mr={2}>
                     Try:
                   </Text>
-                  {sample_queries.map(query => {
+                  {sample_queries.map((query) => {
                     return (
                       <QuickQueryLink
                         key={query.title}
                         title={query.title}
-                        queryString={query.searchTerms.join(' OR ')}
+                        queryString={query.searchTerms.join(" OR ")}
                       />
                     );
                   })}
@@ -285,36 +288,37 @@ const Home: NextPage = () => {
         </PageContent>
 
         {/* NIAID Data Ecosystem section */}
-        <PageContent justifyContent='center' bg='white' minH='unset'>
+        <PageContent justifyContent="center" bg="white" minH="unset">
           <StyledSection
-            id='nde'
-            alignItems='center'
-            flexDirection='column'
-            maxWidth={['100%', '100%', '750px']}
+            id="nde"
+            alignItems="center"
+            flexDirection="column"
+            maxWidth={["100%", "100%", "750px"]}
           >
             <StyledSectionHeading>
               {homepageCopy.sections[1].heading}
             </StyledSectionHeading>
 
             <StyledText
-              textAlign={['start', 'center']}
+              textAlign={["start", "center"]}
               mt={4}
-              fontSize={['lg', 'xl']}
-              lineHeight='taller'
-              maxW='unset'
+              fontSize={["lg", "xl"]}
+              lineHeight="taller"
+              maxW="unset"
             >
               {homepageCopy.sections[1].body}
             </StyledText>
-            <StyledSectionButtonGroup variant='solid' justifyContent='center'>
+            <StyledSectionButtonGroup variant="solid" justifyContent="center">
               {homepageCopy.sections[1]?.routes &&
-                homepageCopy.sections[1].routes.map(route => {
+                homepageCopy.sections[1].routes.map((route) => {
                   return (
                     <Button
                       key={route.title}
-                      href={`${env.BASE_URL}${route.path}`}
-                      w='100%'
+                      href={`${route.path}`}
+                      w="100%"
                       my={2}
-                      target='_self'
+                      target="_self"
+                      isExternal={route.isExternal || false}
                     >
                       {route.title}
                     </Button>
@@ -327,17 +331,17 @@ const Home: NextPage = () => {
         {/* Display stats about the Biothings API */}
         {!error && (
           <PageContent
-            w='100%'
-            bg='white'
-            minH='unset'
-            flexDirection='column'
-            justifyContent='space-around'
-            alignItems='center'
+            w="100%"
+            bg="white"
+            minH="unset"
+            flexDirection="column"
+            justifyContent="space-around"
+            alignItems="center"
             py={[6, 10]}
           >
             <SimpleGrid
               columns={[1, 2, Object.values(stats).length]}
-              w='100%'
+              w="100%"
               spacing={[6, 8, 4]}
             >
               {Object.values(stats).map((stat, i) => {
@@ -345,26 +349,26 @@ const Home: NextPage = () => {
                   <LoadingSpinner key={i} isLoading={isLoading}>
                     {stat?.term && (
                       <Flex
-                        alignItems='center'
-                        flexDirection='column'
-                        textAlign='center'
+                        alignItems="center"
+                        flexDirection="column"
+                        textAlign="center"
                       >
                         <Image
                           src={`${assetPrefix}/assets/${stat.term
                             .toLowerCase()
-                            .replaceAll(' ', '-')}.svg`}
+                            .replaceAll(" ", "-")}.svg`}
                           alt={`Icon for ${stat.term}`}
-                          boxSize='50px'
-                          objectFit='contain'
+                          boxSize="50px"
+                          objectFit="contain"
                           mb={1}
                         />
-                        <Heading size='md' fontWeight='bold' my={1}>
+                        <Heading size="md" fontWeight="bold" my={1}>
                           {formatNumber(stat.count)}
                         </Heading>
                         <Heading
-                          size='xs'
-                          fontWeight='medium'
-                          lineHeight='shorter'
+                          size="xs"
+                          fontWeight="medium"
+                          lineHeight="shorter"
                         >
                           {stat.term}
                         </Heading>
@@ -379,12 +383,12 @@ const Home: NextPage = () => {
 
         {/* Data repository viz section */}
         <PageContent
-          bg='page.alt'
-          minH='unset'
-          flexDirection='column'
-          alignItems='center'
+          bg="page.alt"
+          minH="unset"
+          flexDirection="column"
+          alignItems="center"
         >
-          <StyledSection id='explore-date'>
+          <StyledSection id="explore-date">
             <LoadingSpinner isLoading={isLoading}>
               {/* Pie chart with number repositories and associated resources*/}
               {stats?.repositories?.stats && (
@@ -392,7 +396,7 @@ const Home: NextPage = () => {
                   width={size || 200}
                   height={size || 200}
                   data={stats.repositories.stats.sort(
-                    (a, b) => a.count - b.count,
+                    (a, b) => a.count - b.count
                   )}
                 ></PieChart>
               )}
@@ -413,15 +417,15 @@ const Home: NextPage = () => {
                       <StyledSectionButtonGroup key={route.title}>
                         <Button
                           href={route.path}
-                          w='100%'
-                          variant='outline'
+                          w="100%"
+                          variant="outline"
                           isExternal={route.isExternal || false}
                         >
                           {route.title}
                         </Button>
                       </StyledSectionButtonGroup>
                     );
-                  },
+                  }
                 )}
             </StyledBody>
           </StyledSection>
@@ -429,18 +433,18 @@ const Home: NextPage = () => {
 
         {/* Connect to the workspace section */}
         <PageContent
-          bg='#fff'
-          minH='unset'
-          flexDirection='column'
-          alignItems='center'
+          bg="#fff"
+          minH="unset"
+          flexDirection="column"
+          alignItems="center"
         >
-          <StyledSection id='workspace' flexDirection={{md: 'row-reverse'}}>
+          <StyledSection id="workspace" flexDirection={{ md: "row-reverse" }}>
             <Image
-              w='100%'
+              w="100%"
               p={4}
-              maxW={{base: 300, xl: 400}}
+              maxW={{ base: 300, xl: 400 }}
               src={`${assetPrefix}/assets/home-analyze.png`}
-              alt={''}
+              alt={""}
             ></Image>
             <StyledBody>
               <StyledSectionHeading mt={[4, 6]}>
@@ -448,10 +452,10 @@ const Home: NextPage = () => {
               </StyledSectionHeading>
               <StyledText>{homepageCopy.sections[3].body}</StyledText>
               {homepageCopy.sections[3]?.routes &&
-                homepageCopy.sections[3].routes.map(route => {
+                homepageCopy.sections[3].routes.map((route) => {
                   return (
                     <StyledSectionButtonGroup key={route.title}>
-                      <Button href={route.path} w='100%' isExternal>
+                      <Button href={route.path} w="100%" isExternal>
                         {route.title}
                       </Button>
                     </StyledSectionButtonGroup>
