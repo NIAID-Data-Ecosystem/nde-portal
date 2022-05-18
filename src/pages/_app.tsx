1c0cd989c4879000d74275b66cbc5dd3a065f44a
candicecz <czech.candice@gmail.com>
2022-05-18 12:44:23 -0400

    [ADD] google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,52 @@
-import type {AppProps} from 'next/app';
-import {QueryClient, QueryClientProvider} from 'react-query';
-import {ThemeProvider} from 'nde-design-system';
-import FontFace from 'src/theme/font-face';
-import Head from 'next/head';
+import { useEffect } from "react";
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import Script from "next/script";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ThemeProvider } from "nde-design-system";
+import FontFace from "src/theme/font-face";
+import { gtmVirtualPageView } from "lib/ga";
 
 // Creates an instance of react-query for the app.
 const queryClient = new QueryClient();
 
-function App({Component, pageProps}: AppProps) {
+function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const mainDataLayer = {
+      pageTypeName: pageProps.page || null,
+      url: router.pathname,
+      query: router.query,
+    };
+
+    gtmVirtualPageView(mainDataLayer);
+  }, [pageProps, router]);
+
   return (
     <>
       <Head>
         <meta
-          name='viewport'
-          content='width=device-width, initial-scale=1.0'
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
         ></meta>
       </Head>
+
+      {/* <!-- Google Tag Manager --> */}
+      <Script
+        id="google-tag-manager"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-K8WGDTD')`,
+        }}
+      ></Script>
+      {/* <!-- End Google Tag Manager --> */}
+
       <QueryClientProvider client={queryClient}>
         <ThemeProvider>
           <FontFace />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,11 +4,13 @@ import NextDocument, {
   Html,
   Main,
   NextScript,
-} from 'next/document';
-import React from 'react';
-import {assetPrefix} from 'next.config';
-import {Box} from 'nde-design-system';
-import {NAV_HEIGHT} from 'src/components/page-container';
+} from "next/document";
+// eslint-disable-next-line @next/next/no-script-in-document
+import Script from "next/script";
+import React from "react";
+import { assetPrefix } from "next.config";
+import { Box } from "nde-design-system";
+import { NAV_HEIGHT } from "src/components/page-container";
 
 class Document extends NextDocument {
   static getInitialProps(ctx: DocumentContext) {
@@ -18,18 +20,25 @@ class Document extends NextDocument {
     return (
       <Box
         as={Html}
-        lang='en'
+        lang="en"
         // put padding for scroll to sections that equals the nav height
         sx={{
           scrollPaddingTop: Object.values(NAV_HEIGHT),
-          scrollBehavior: 'smooth',
+          scrollBehavior: "smooth",
         }}
       >
         <Head>
-          <style data-href='https://fonts.googleapis.com/css2?family=Public+Sans:wght@100;200;300;400;500;600;700;800;900'></style>
-          <link rel='icon' href={`${assetPrefix}/favicon.png`} />
+          <style data-href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@100;200;300;400;500;600;700;800;900"></style>
+          <link rel="icon" href={`${assetPrefix}/favicon.png`} />
         </Head>
         <body>
+          {/* <!-- Google Tag Manager (noscript) --> */}
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K8WGDTD" height="0" width="0" style="display: none; visibility: hidden;" />`,
+            }}
+          />
+          {/* <!-- End Google Tag Manager (noscript) --> */}
           <Main />
           <NextScript />
         </body>
