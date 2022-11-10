378232dbff5dd94689301fb09ee426932d5a761d
candicecz <czech.candice@gmail.com>
2022-11-10 14:22:00 -0400

    [FIX] eslint

--- src/theme/font-face.tsx
+++ src/theme/font-face.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const FontFace = () => (
+  // eslint-disable-next-line react/no-unknown-property
   <style jsx global>
     {`
       @import url('https://fonts.googleapis.com/css2?family=Public+Sans:wght@100;200;300;400;500;600;700;800;900');
