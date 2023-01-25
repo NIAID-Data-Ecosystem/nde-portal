25891eb5f70ff09e0a6bcf10ab39dc0aa19c2c5f
candicecz <czech.candice@gmail.com>
2023-01-25 12:23:53 -0400

    [REMOVE] name sorting

--- src/components/search-results-page/index.tsx
+++ src/components/search-results-page/index.tsx
@@ -48,8 +48,8 @@ export const sortOptions = [
   { name: 'Best Match', sortBy: '_score', orderBy: 'asc' },
   { name: 'Date: oldest to newest', sortBy: 'date', orderBy: 'asc' },
   { name: 'Date: newest to oldest', sortBy: 'date', orderBy: 'desc' },
-  { name: 'A-Z', sortBy: 'name.raw', orderBy: 'asc' },
-  { name: 'Z-A', sortBy: 'name.raw', orderBy: 'desc' },
+  // { name: 'A-Z', sortBy: 'name.raw', orderBy: 'asc' },
+  // { name: 'Z-A', sortBy: 'name.raw', orderBy: 'desc' },
 ] as const;
 
 export interface SortOptionsInterface {
