d4d4ac5afceb9363e5cd984233c887022675cd97
candicecz <czech.candice@gmail.com>
2023-01-25 12:27:56 -0400

    [UPDATE] sources alternateName

--- src/components/search-results-page/index.tsx
+++ src/components/search-results-page/index.tsx
@@ -48,6 +48,7 @@ export const sortOptions = [
   { name: 'Best Match', sortBy: '_score', orderBy: 'asc' },
   { name: 'Date: oldest to newest', sortBy: 'date', orderBy: 'asc' },
   { name: 'Date: newest to oldest', sortBy: 'date', orderBy: 'desc' },
+  // [TO DO]: Add once prod has been updated.
   // { name: 'A-Z', sortBy: 'name.raw', orderBy: 'asc' },
   // { name: 'Z-A', sortBy: 'name.raw', orderBy: 'desc' },
 ] as const;
