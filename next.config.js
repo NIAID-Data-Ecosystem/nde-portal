77edf1018c23ae1f445a1fb0a3d42bd1d3ea22c9
candicecz <czech.candice@gmail.com>
2023-02-28 12:26:02 -0400

    chore(packages): fix vulnerabilities

--- next.config.js
+++ next.config.js
@@ -20,11 +20,20 @@ module.exports = withMDX({
   trailingSlash: true,
   assetPrefix: isProd ? process.env.BASE_URL : undefined,
   basePath: isProd ? process.env.BASE_URL : '',
-  webpack: (config, { isServer }) => {
+  webpack: (config, { isServer, webpack }) => {
     // Fixes npm packages (mdx) that depend on `fs` module
     if (!isServer) {
       config.resolve.fallback.fs = false;
     }
+
+    config.plugins.push(
+      // Remove node: from import specifiers, because Next.js does not yet support node: scheme
+      // https://github.com/vercel/next.js/issues/28774
+      new webpack.NormalModuleReplacementPlugin(/^node:/, resource => {
+        resource.request = resource.request.replace(/^node:/, '');
+      }),
+    );
+
     return {
       ...config,
 
