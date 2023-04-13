3c6d62fd24a9ba05a3d96399702f96241a392a28
Dylan Welzel <dylanwelzel@gmail.com>
2023-04-13 11:17:46 -0700

    fix: running locally

--- tests/check-links.py
+++ tests/check-links.py
@@ -8,11 +8,11 @@ from selenium import webdriver
 
 PROD = os.environ.get('PROD', False)
 
-
+start = time.now()
 if PROD:
     base_url = 'https://data.niaid.nih.gov'
 else:
-    base_url = 'https://data-staging.niaid.nih.gov'
+    base_url = 'http://localhost:3000'
 
 site_links = [
     '/',
@@ -36,7 +36,7 @@ broken_links = []
 
 for site_link in site_links:
     full_url = f"{base_url}{site_link}"
-    print(f'Current page: {full_url}')
+    logging.info(f'Current page: {full_url}')
 
     driver.get(full_url)
 
@@ -47,14 +47,16 @@ for site_link in site_links:
     for link in links:
         href = link.get_attribute("href")
         if href is not None and "http" in href:
-            print(f'Checking {href}...')
+            if 'altmetric' in href:
+                continue
+            logging.info(f'Checking {href}...')
             headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'}
             response = requests.get(href,headers=headers, allow_redirects=True)
             try:
                 response.raise_for_status()
             except requests.exceptions.HTTPError as e:
-                print(e)
-                print(f"{href} is broken")
+                logging.info(e)
+                logging.info(f"{href} is broken")
                 broken_links.append(href)
 
 
@@ -62,6 +64,8 @@ driver.close()
 
 log_file_name = f"broken_links_{datetime.today().strftime('%Y-%m-%d')}.log"
 logging.basicConfig(filename=log_file_name, level=logging.DEBUG, format='%(asctime)s %(message)s')
+end = time.end()
+broken_links.append(f"Total time: {end - start}")
 for link in set(broken_links):
     logging.error(link)
 
