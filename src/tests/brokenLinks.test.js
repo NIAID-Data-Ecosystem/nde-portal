bb90dd854a8d1481fa346016a62a042f9c9fa832
Dylan Welzel <dylanwelzel@gmail.com>
2023-04-18 10:20:00 -0700

    fix: code cleanup

--- src/tests/brokenLinks.test.js
+++ src/tests/brokenLinks.test.js
@@ -1,16 +1,81 @@
 const axios = require('axios');
 const puppeteer = require('puppeteer');
-const core = require('@actions/core');
+
+const buildSlackBlocks = () => {
+  const dividerBlock = {
+    type: 'divider',
+  };
+
+  const blocks = [dividerBlock];
+
+  for (const brokenLink of brokenLinks) {
+    const [url, page, error] = brokenLink.split(' | ');
+
+    const linkBlock = {
+      type: 'section',
+      fields: [
+        {
+          type: 'mrkdwn',
+          text: `*Broken URL:*\n<${url}|${url}>`,
+        },
+        {
+          type: 'mrkdwn',
+          text: `*Page:*\n${page}`,
+        },
+        {
+          type: 'mrkdwn',
+          text: `*Error Code:*\n${error}`,
+        },
+      ],
+    };
+
+    blocks.push(linkBlock, dividerBlock);
+  }
+
+  return blocks;
+};
+
+const sendSlackMessage = async blocks => {
+  const SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL;
+  if (!SLACK_WEBHOOK_URL) {
+    console.error('SLACK_WEBHOOK_URL is not set');
+    return;
+  }
+
+  const headerText = `:boom: ${brokenLinks.length} link${
+    brokenLinks.length > 1 ? 's' : ''
+  } broken out of ${savedLinks.length} in ${process.env.GITHUB_REPOSITORY}`;
+
+  const headerBlock = {
+    type: 'section',
+    text: {
+      type: 'mrkdwn',
+      text: headerText,
+    },
+  };
+
+  blocks.unshift(headerBlock);
+
+  const payload = {
+    blocks: blocks,
+  };
+
+  try {
+    await axios.post(SLACK_WEBHOOK_URL, payload);
+  } catch (error) {
+    console.error(`Failed to send Slack message: ${error}`);
+  }
+};
 
 const baseURL = 'http://localhost:3000';
 const siteLinks = [
   '/',
-  // '/search/',
-  // '/changelog/',
-  // '/sources/',
-  // '/advanced-search/',
-  // '/about/',
-  // '/faq/',
+  '/search/',
+  '/changelog/',
+  '/sources/',
+  '/advanced-search/',
+  '/about/',
+  '/faq/',
 ];
 
 let brokenLinks = [];
@@ -45,26 +110,17 @@ describe('Check for broken links', () => {
           href &&
           href.startsWith('http') &&
           !href.includes('altmetric') &&
-          !savedLinks.includes(href)
+          !savedLinks.some(item => item.href === href)
         ) {
           savedLinks.push({ href, siteLink });
         }
       }
-      savedLinks = [
-        {
-          href: 'https://dash.readme.com/project/niaid-data/v1.0/docs/how-do-i-filter-results-by',
-          siteLink: '/',
-        },
-        { href: 'https://www.this.is.broken.com', siteLink: '/sources/' },
-      ];
     }
-
     console.log(`Found ${savedLinks.length} links to check...`);
   }, 60000);
   test('Check all gathered links', async () => {
     for (const obj of savedLinks) {
       href = obj.href;
-      console.log(href, 'href');
       console.log(`Checking ${href}`);
       await axios
         .get(href, {
@@ -75,9 +131,9 @@ describe('Check for broken links', () => {
           maxRedirects: 10,
         })
         .catch(error => {
-          const message = `${href} - ${obj.siteLink} (${
-            error.response ? error.response.status : error
-          })`;
+          const message = `${href} | ${obj.siteLink} | ${
+            error.response ? error.response.status : error.message
+          }`;
           if (error.response) {
             console.error(
               `${href} is broken with status ${error.response.status} on page ${obj.siteLink}`,
@@ -89,38 +145,11 @@ describe('Check for broken links', () => {
           }
           brokenLinks.push(message);
         });
-      // .catch(error => {
-      //   if (error.response) {
-      //     console.error(
-      //       `${href} is broken with status ${error.response.status}`,
-      //     );
-      //     // brokenLinks.push(`${href} (${error.response.status})`);
-      //     brokenLinks.push(
-      //       `| ${href} | ${obj.siteLink} | ${error.response.status} |`,
-      //     );
-      //   } else {
-      //     console.error(`${href} is broken with ${error}`);
-      //     // brokenLinks.push(`${href} (${error})`);
-      //     brokenLinks.push(`| ${href} | ${obj.siteLink} | ${error} |`);
-      //   }
-      // });
     }
-    // if (brokenLinks.length > 0) {
-    //   const logFileName = 'broken_links.log';
-    //   const logContent = brokenLinks.join('\n');
 
-    //   fs.writeFileSync(logFileName, logContent, 'utf-8', err => {
-    //     if (err) {
-    //       console.error(`Failed to write log file: ${err}`);
-    //     }
-    //   });
-
-    //   console.log(`Broken links logged to ${logFileName}`);
-    // }
     if (brokenLinks.length > 0) {
-      const logContent = brokenLinks.join('\\n');
-      core.setOutput('broken_links', logContent);
-      console.log(`Broken links: \n${logContent}`);
+      const blocks = buildSlackBlocks();
+      await sendSlackMessage(blocks);
     }
 
     expect(brokenLinks).toEqual([]);
