773b7cb53258b085ce5228e7e2673abd009fd4e8
candicecz <czech.candice@gmail.com>
2023-06-15 12:42:37 -0300

    fix: advanced search disclaimer local storage
    style: advanced search disclaimer

--- src/components/advanced-search/components/Search/components/Disclaimer/index.tsx
+++ src/components/advanced-search/components/Search/components/Disclaimer/index.tsx
@@ -1,7 +1,9 @@
+import React, { useEffect, useState } from 'react';
 import { MetadataIcon } from 'src/components/icon';
-import { Box, Button, Flex, SlideFade, Text } from 'nde-design-system';
+import { Box, Button, Flex, Icon, SlideFade, Text } from 'nde-design-system';
 import { useAdvancedSearchContext } from '../AdvancedSearchFormContext';
 import { useLocalStorage } from 'usehooks-ts';
+import { IoWarningOutline } from 'react-icons/io5';
 
 export const Disclaimer = () => {
   const { selectedSearchType } = useAdvancedSearchContext();
@@ -10,55 +12,77 @@ export const Disclaimer = () => {
     false,
   );
 
+  const [isMounted, setIsMounted] = useState(false); // local storage for SSR.
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
   return (
     <Flex
-      opacity={selectedSearchType?.additionalInfo ? 1 : 0}
+      visibility={selectedSearchType?.additionalInfo ? 'visible' : 'hidden'}
       w='100%'
       justifyContent='flex-end'
       alignItems='center'
       px={2}
       mt={1}
     >
-      <Button
-        size='sm'
-        variant='ghost'
-        onClick={() => setIsMinimized(!isMinimized)}
-        _active={{ boxShadow: 'none' }}
-        _focus={{ boxShadow: 'none' }}
-        px={2}
-      >
-        <Box mr={isMinimized ? 1 : 0}>
-          <MetadataIcon id='info' glyph='info' fill='gray.700' />
-        </Box>
-        {isMinimized && <Text fontSize='sm'>About this query</Text>}
-      </Button>
-
-      <SlideFade
-        in={!isMinimized && !!selectedSearchType?.additionalInfo}
-        offsetX={20}
-        offsetY={0}
-      >
-        <Flex
-          display={isMinimized ? 'none' : 'flex'}
-          alignItems='center'
-          justifyContent='flex-end'
-          py={1}
-        >
-          <Text fontStyle='italic' fontWeight='light' fontSize='sm'>
-            {selectedSearchType.additionalInfo}
-          </Text>
+      {isMounted ? (
+        <Flex bg='status.warning_lt' alignItems='center'>
           <Button
-            variant='ghost'
-            onClick={() => setIsMinimized(true)}
             size='sm'
-            textDecoration='underline'
+            variant='ghost'
+            onClick={() => setIsMinimized(!isMinimized)}
             px={2}
-            mx={1}
+            position='unset'
+            bg={isMinimized ? 'status.warning' : 'status.warning_lt'}
+            borderColor='status.warning'
+            color='text.heading'
+            _hover={{ bg: 'status.warning_lt' }}
+            _active={{ boxShadow: 'none' }}
+            _focus={{ boxShadow: 'none' }}
           >
-            Got it
+            <Flex mx={1}>
+              <Icon as={IoWarningOutline} color='inherit' />
+            </Flex>
+            {isMinimized && (
+              <Text fontSize='sm' color='inherit'>
+                About this query
+              </Text>
+            )}
           </Button>
+          <SlideFade
+            in={!isMinimized && !!selectedSearchType?.additionalInfo}
+            offsetX={20}
+            offsetY={0}
+          >
+            <Flex
+              display={isMinimized ? 'none' : 'flex'}
+              alignItems='center'
+              justifyContent='flex-end'
+            >
+              <Text fontStyle='italic' fontWeight='light' fontSize='sm'>
+                {selectedSearchType.additionalInfo}
+              </Text>
+              <Button
+                variant='ghost'
+                onClick={() => setIsMinimized(true)}
+                size='sm'
+                textDecoration='underline'
+                px={2}
+                mx={1}
+                position='unset'
+                _hover={{ bg: 'status.warning' }}
+                _active={{ bg: 'status.warning', boxShadow: 'none' }}
+              >
+                Got it
+              </Button>
+            </Flex>
+          </SlideFade>
         </Flex>
-      </SlideFade>
+      ) : (
+        <></>
+      )}
     </Flex>
   );
 };
