322e4a600ab31f5335fa7698a81ce159729a3ce6
candicecz <czech.candice@gmail.com>
2023-07-07 11:52:10 -0300

    chore: update 404 image

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -15,6 +15,7 @@ const NotFoundPage: NextPage = () => {
         <Flex flexDirection='column' alignItems='center'>
           <Image
             w='80%'
+            h='auto'
             src='/assets/404.webp'
             alt='404: Page Not Found'
             mb={8}
