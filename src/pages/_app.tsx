8519ddee36f6f7f4fbf1b85bcb4b4cfb002925dc
candicecz <czech.candice@gmail.com>
2023-09-25 10:50:50 -0400

    fix: dap analytics for all paths

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -39,16 +39,8 @@ function App({ Component, pageProps }: AppProps) {
     const handleRouteChange = (url: string) => {
       ga.pageview(url);
     };
-    //When the component is mounted, subscribe to router changes
-    //and log those page views
-    router.events.on('routeChangeComplete', handleRouteChange);
-
-    // If the component is unmounted, unsubscribe
-    // from the event with the `off` method
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
+    handleRouteChange(router.pathname);
+  }, [router]);
 
   useEffect(() => {
     if (router.query && router.query.q) {
