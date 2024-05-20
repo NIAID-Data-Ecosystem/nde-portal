8e1917e20e2047163c7ba8da8deab683a42333d6
candicecz <czech.candice@gmail.com>
2024-05-20 11:35:54 -0400

    wip: add zoom container

--- src/components/resource-sections/components/topics/index.tsx
+++ src/components/resource-sections/components/topics/index.tsx
@@ -1,7 +1,9 @@
 import { TopicBrowser } from 'src/components/topic-brower';
+import { ZoomContainer } from 'src/components/topic-brower/components/zoom-container';
 import { transformPathArraysToTree } from 'src/components/topic-brower/helpers';
 import { useEDAMPaths2Root } from 'src/components/topic-brower/hooks';
 import { FormattedResource } from 'src/utils/api/types';
+import { ParentSize } from '@visx/responsive';
 
 interface TopicDisplayProps {
   topics: FormattedResource['topicCategory'];
@@ -10,5 +12,21 @@ interface TopicDisplayProps {
 export const TopicDisplay = ({ topics }: TopicDisplayProps) => {
   const { data } = useEDAMPaths2Root(topics);
   const tree = data && transformPathArraysToTree(data.flat());
-  return <TopicBrowser data={tree} />;
+  return (
+    <div>
+      <ParentSize>
+        {parent => {
+          return (
+            <ZoomContainer width={parent.width} height={parent.width * 0.52}>
+              <TopicBrowser
+                data={tree}
+                width={parent.width}
+                height={parent.width * 0.52}
+              />
+            </ZoomContainer>
+          );
+        }}
+      </ParentSize>
+    </div>
+  );
 };
--- src/components/topic-brower/index.tsx
+++ src/components/topic-brower/index.tsx
@@ -1,14 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { TreeNode } from './helpers';
-import getLinkComponent from './getLinkComponent';
-import LinkControls from './LinkControls';
 import { LinearGradient } from '@visx/gradient';
 import { Group } from '@visx/group';
 import { hierarchy, Tree } from '@visx/hierarchy';
-import { pointRadial } from 'd3-shape';
+import { LinkHorizontal } from '@visx/shape';
 
 // For Zoom component: https://airbnb.io/visx/zoom-i
-const defaultMargin = { top: 30, left: 50, right: 50, bottom: 70 };
+const defaultMargin = { top: 50, left: 80, right: 100, bottom: 50 };
 
 export const TopicBrowser = ({
   data: tree,
@@ -18,150 +16,104 @@ export const TopicBrowser = ({
 }: {
   data: TreeNode;
   margin?: { top: number; right: number; bottom: number; left: number };
-  width: number;
-  height: number;
+  width?: number;
+  height?: number;
 }) => {
-  const data = tree.children[0];
-  const [layout, setLayout] = useState<string>('cartesian');
-  const [orientation, setOrientation] = useState<string>('horizontal');
-  const [linkType, setLinkType] = useState<string>('diagonal');
-  const [stepPercent, setStepPercent] = useState<number>(0.5);
+  const data = tree?.children?.[0];
+
   // const forceUpdate = useForceUpdate();
 
   const innerWidth = totalWidth - margin.left - margin.right;
   const innerHeight = totalHeight - margin.top - margin.bottom;
 
-  let origin: { x: number; y: number };
-  let sizeWidth: number;
-  let sizeHeight: number;
+  const origin = { x: 0, y: 0 };
+  const sizeWidth = innerHeight;
+  const sizeHeight = innerWidth;
 
-  if (layout === 'polar') {
-    origin = {
-      x: innerWidth / 2,
-      y: innerHeight / 2,
-    };
-    sizeWidth = 2 * Math.PI;
-    sizeHeight = Math.min(innerWidth, innerHeight) / 2;
-  } else {
-    origin = { x: 0, y: 0 };
-    if (orientation === 'vertical') {
-      sizeWidth = innerWidth;
-      sizeHeight = innerHeight;
-    } else {
-      sizeWidth = innerHeight;
-      sizeHeight = innerWidth;
-    }
-  }
-
-  const LinkComponent = getLinkComponent({ layout, linkType, orientation });
   return !data || totalWidth < 10 ? null : (
-    <div>
-      <LinkControls
-        layout={layout}
-        orientation={orientation}
-        linkType={linkType}
-        stepPercent={stepPercent}
-        setLayout={setLayout}
-        setOrientation={setOrientation}
-        setLinkType={setLinkType}
-        setStepPercent={setStepPercent}
-      />
-      <svg width={totalWidth} height={totalHeight}>
-        <LinearGradient id='links-gradient' from='#fd9b93' to='#fe6e9e' />
-        <rect width={totalWidth} height={totalHeight} rx={14} fill='#272b4d' />
-        <Group top={margin.top} left={margin.left}>
-          <Tree
-            root={hierarchy(data, d => (d.isExpanded ? null : d.children))}
-            size={[sizeWidth, sizeHeight]}
-            separation={(a, b) => (a.parent === b.parent ? 1 : 0.5) / a.depth}
-          >
-            {tree => (
-              <Group top={origin.y} left={origin.x}>
-                {tree.links().map((link, i) => (
-                  <LinkComponent
-                    key={i}
-                    data={link}
-                    percent={stepPercent}
-                    stroke='rgb(254,110,158,0.6)'
-                    strokeWidth='1'
-                    fill='none'
-                  />
-                ))}
+    <svg width={totalWidth} height={totalHeight}>
+      <LinearGradient id='links-gradient' from='#fd9b93' to='#fe6e9e' />
+      <rect width={totalWidth} height={totalHeight} rx={14} fill='#272b4d' />
+      <Group top={margin.top} left={margin.left}>
+        <Tree
+          root={hierarchy(data, d => (d.isExpanded ? null : d.children))}
+          size={[sizeWidth, sizeHeight]}
+          separation={(a, b) => (a.parent === b.parent ? 1 : 0.5) / a.depth}
+        >
+          {tree => (
+            <Group top={origin.y} left={origin.x}>
+              {tree.links().map((link, i) => (
+                <LinkHorizontal
+                  key={i}
+                  data={link}
+                  stroke='rgb(254,110,158,0.6)'
+                  strokeWidth='1'
+                  fill='none'
+                />
+              ))}
 
-                {tree.descendants().map((node, key) => {
-                  const width = 40;
-                  const height = 20;
+              {tree.descendants().map((node, key) => {
+                const width = 40;
+                const height = 20;
 
-                  let top: number;
-                  let left: number;
-                  if (layout === 'polar') {
-                    const [radialX, radialY] = pointRadial(node.x, node.y);
-                    top = radialY;
-                    left = radialX;
-                  } else if (orientation === 'vertical') {
-                    top = node.y;
-                    left = node.x;
-                  } else {
-                    top = node.x;
-                    left = node.y;
-                  }
+                let top = node.x;
+                let left = node.y;
 
-                  return (
-                    <Group top={top} left={left} key={key}>
-                      {node.depth === 0 && (
-                        <circle
-                          r={12}
-                          fill="url('#links-gradient')"
-                          // onClick={() => {
-                          //   node.data.isExpanded = !node.data.isExpanded;
-                          //   // console.log(node);
-                          //   // forceUpdate();
-                          // }}
-                        />
-                      )}
-                      {node.depth !== 0 && (
-                        <rect
-                          height={height}
-                          width={width}
-                          y={-height / 2}
-                          x={-width / 2}
-                          fill='#272b4d'
-                          stroke={node.data.children ? '#03c0dc' : '#26deb0'}
-                          strokeWidth={1}
-                          strokeDasharray={node.data.children ? '0' : '2,2'}
-                          strokeOpacity={node.data.children ? 1 : 0.6}
-                          rx={node.data.children ? 0 : 10}
-                          // onClick={() => {
-                          //   node.data.isExpanded = !node.data.isExpanded;
-                          //   // console.log(node);
-                          //   // forceUpdate();
-                          // }}
-                        />
-                      )}
-                      <text
-                        dy='.33em'
-                        fontSize={9}
-                        fontFamily='Arial'
-                        textAnchor='middle'
-                        style={{ pointerEvents: 'none' }}
-                        fill={
-                          node.depth === 0
-                            ? '#71248e'
-                            : node.children
-                            ? 'white'
-                            : '#26deb0'
-                        }
-                      >
-                        {node.data.name}
-                      </text>
-                    </Group>
-                  );
-                })}
-              </Group>
-            )}
-          </Tree>
-        </Group>
-      </svg>
-    </div>
+                return (
+                  <Group top={top} left={left} key={key}>
+                    {node.depth === 0 && (
+                      <circle
+                        r={12}
+                        fill="url('#links-gradient')"
+                        // onClick={() => {
+                        //   node.data.isExpanded = !node.data.isExpanded;
+                        //   // console.log(node);
+                        //   // forceUpdate();
+                        // }}
+                      />
+                    )}
+                    {node.depth !== 0 && (
+                      <rect
+                        height={height}
+                        width={width}
+                        y={-height / 2}
+                        x={-width / 2}
+                        fill='#272b4d'
+                        stroke={node.data.children ? '#03c0dc' : '#26deb0'}
+                        strokeWidth={1}
+                        strokeDasharray={node.data.children ? '0' : '2,2'}
+                        strokeOpacity={node.data.children ? 1 : 0.6}
+                        rx={node.data.children ? 0 : 10}
+                        // onClick={() => {
+                        //   node.data.isExpanded = !node.data.isExpanded;
+                        //   // console.log(node);
+                        //   // forceUpdate();
+                        // }}
+                      />
+                    )}
+                    <text
+                      dy='.33em'
+                      fontSize={9}
+                      fontFamily='Arial'
+                      textAnchor='middle'
+                      style={{ pointerEvents: 'none' }}
+                      fill={
+                        node.depth === 0
+                          ? '#71248e'
+                          : node.children
+                          ? 'white'
+                          : '#26deb0'
+                      }
+                    >
+                      {node.data.name}
+                    </text>
+                  </Group>
+                );
+              })}
+            </Group>
+          )}
+        </Tree>
+      </Group>
+    </svg>
   );
 };
