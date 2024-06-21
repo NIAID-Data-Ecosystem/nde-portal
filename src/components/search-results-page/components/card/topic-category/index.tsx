7bd67de876d6dd074197d7f4e9ecbbc1de8f022c
candicecz <czech.candice@gmail.com>
2024-06-21 13:19:39 -0400

    style: change placement of topic category label

--- src/components/search-results-page/components/card/topic-category/index.tsx
+++ src/components/search-results-page/components/card/topic-category/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Flex, Text } from '@chakra-ui/react';
+import { Flex, Text } from '@chakra-ui/react';
 import { TopicCategory } from 'src/utils/api/types';
 import { FaMagnifyingGlass } from 'react-icons/fa6';
 import { TagWithUrl } from 'src/components/tag-with-url';
@@ -16,37 +16,37 @@ const TopicCategories: React.FC<TopicCategoryProps> = ({ data }) => {
   const paddingCard = [4, 6, 8, 10];
   if (topicCategoryNames?.length === 0) return null;
   return (
-    <Box
+    <Flex
+      flexWrap='wrap'
       my={0}
       px={paddingCard}
       py={1}
       borderBottom='1px solid'
       borderBottomColor='gray.200'
     >
-      <Text fontWeight='medium' fontSize='xs' color='gray.800'>
-        Topic Categories
+      <Text fontSize='xs' color='gray.800' mr={1}>
+        Topic Categories:
       </Text>
-      <Flex flexWrap='wrap'>
-        {topicCategoryNames?.map(name => {
-          return (
-            <TagWithUrl
-              key={name}
-              colorScheme='primary'
-              href={{
-                pathname: '/search',
-                query: {
-                  q: `topicCategory.name:"${name.trim()}"`,
-                },
-              }}
-              m={0.5}
-              leftIcon={FaMagnifyingGlass}
-            >
-              {name}
-            </TagWithUrl>
-          );
-        })}
-      </Flex>
-    </Box>
+
+      {topicCategoryNames?.map(name => {
+        return (
+          <TagWithUrl
+            key={name}
+            colorScheme='primary'
+            href={{
+              pathname: '/search',
+              query: {
+                q: `topicCategory.name:"${name.trim()}"`,
+              },
+            }}
+            m={0.5}
+            leftIcon={FaMagnifyingGlass}
+          >
+            {name}
+          </TagWithUrl>
+        );
+      })}
+    </Flex>
   );
 };
 
