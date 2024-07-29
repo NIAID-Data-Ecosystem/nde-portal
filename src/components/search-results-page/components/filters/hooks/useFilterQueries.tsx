845d65d133a6c5fe012c8f62714814faeea1cca5
candicecz <czech.candice@gmail.com>
2024-07-29 15:42:27 -0400

    refactor(search): useFiltersQuery combine query

--- src/components/search-results-page/components/filters/hooks/useFilterQueries.tsx
+++ src/components/search-results-page/components/filters/hooks/useFilterQueries.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useState } from 'react';
-import { useQueries } from '@tanstack/react-query';
+import { useQueries, UseQueryResult } from '@tanstack/react-query';
 import { Params } from 'src/utils/api';
 import { FILTER_CONFIGS } from '../helpers';
 import { TransformedQueryResult } from '../types';
@@ -60,6 +60,26 @@ const mergeResults = (
 };
 
 /**
+ * Combines query results and determines if any query is loading.
+ *
+ * @param queryResult - The array of query results.
+ * @returns An object containing the combined data and the loading state.
+ */
+const combineQueryResults = (
+  queryResult: UseQueryResult<TransformedQueryResult, Error>[],
+): { data: QueryResultAccumulator; isLoading: boolean } => {
+  const isLoading = queryResult.some(query => query.isLoading);
+
+  const results = queryResult.reduce((acc, { data }) => {
+    if (!data || !data?.facet) return acc;
+    const { facet, results } = data;
+    acc[facet] = acc[facet] ? acc[facet].concat(results) : results;
+    return acc;
+  }, {} as QueryResultAccumulator);
+  return { data: results, isLoading };
+};
+
+/**
  * Custom hook to manage filter queries.
  *
  * This hook handles the creation, execution, and merging of filter queries.
@@ -85,20 +105,8 @@ export const useFilterQueries = (queryParams: Params) => {
   // Fetch the initial results without any extra filters
   const { data: initialResults, isLoading } = useQueries({
     queries: initialQueries,
-    combine: (
-      queryResult,
-    ): { data: QueryResultAccumulator; isLoading: boolean } => {
-      const isLoading = queryResult.some(query => query.isLoading);
-      const results = queryResult.reduce((acc, { data }) => {
-        if (!data) return acc;
-        const { facet, results } = data;
-        acc[facet] = acc[facet] ? acc[facet].concat(results) : results;
-        return acc;
-      }, {} as QueryResultAccumulator);
-      return { data: results, isLoading };
-    },
+    combine: combineQueryResults,
   });
-
   // Determine if filtered queries should be enabled i.e. run when the initial results are available and extra filters are selected.
   const enableFilteredQueries = useMemo(
     () =>
@@ -123,19 +131,7 @@ export const useFilterQueries = (queryParams: Params) => {
   // Fetch the updated results with the selected filters
   const { data: filteredResults, isLoading: isUpdating } = useQueries({
     queries: filteredQueries,
-    combine: (
-      queryResult,
-    ): { data: QueryResultAccumulator; isLoading: boolean } => {
-      const isLoading = queryResult.some(query => query.isLoading);
-      const results = queryResult.reduce((acc, { data }) => {
-        if (!data) return acc;
-        const { facet, results } = data;
-        acc[facet] = acc[facet] ? acc[facet].concat(results) : results;
-        return acc;
-      }, {} as QueryResultAccumulator);
-
-      return { data: results, isLoading };
-    },
+    combine: combineQueryResults,
   });
 
   // Merge initial and filtered results (if they exist)
