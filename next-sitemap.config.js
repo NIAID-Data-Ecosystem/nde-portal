32186145434182745a757bbdd96e05be40561fbf
candicecz <czech.candice@gmail.com>
2024-12-10 11:10:29 -0500

    chore: update robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -11,6 +11,7 @@ module.exports = {
         allow: '/',
       },
       { userAgent: 'bingbot', allow: '/' },
+      { userAgent: 'DuckDuckBot', allow: '/' },
     ],
     additionalSitemaps: [`${process.env.BASE_URL}/sitemap-datasets.xml`],
   },
