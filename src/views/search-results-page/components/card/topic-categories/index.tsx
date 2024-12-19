f053bd5389996bb94480a367f3651a9e737f17ef
Leandro Collares <leco@uvic.ca>
2024-12-19 09:36:19 -0300

    feat: create scrollable list of topic categories

--- src/views/search-results-page/components/card/topic-categories/index.tsx
+++ src/views/search-results-page/components/card/topic-categories/index.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Flex, FlexProps, Icon, Text } from '@chakra-ui/react';
 import { FormattedResource, TopicCategory } from 'src/utils/api/types';
 import { FaMagnifyingGlass } from 'react-icons/fa6';
+import { ScrollContainer } from 'src/components/scroll-container';
 import { TagWithUrl } from 'src/components/tag-with-url';
+import { Button } from '@chakra-ui/react';
+import { formatNumber } from 'src/utils/helpers';
 import Tooltip from 'src/components/tooltip';
 import { SchemaDefinitions } from 'scripts/generate-schema-definitions/types';
 import SCHEMA_DEFINITIONS from 'configs/schema-definitions.json';
@@ -12,6 +15,15 @@ interface TopicCategoryProps extends FlexProps {
   data?: TopicCategory[] | null;
   type: FormattedResource['@type'];
 }
+
+// Number of topic categories to be shown when the view is not expanded
+const DEFAULT_LIMIT = 3;
+
+const generateButtonLabel = (limit: number, length: number) => {
+  return limit === length
+    ? 'Show fewer topics'
+    : `Show all topics (${formatNumber(length - limit)} more)`;
+};
 
 const metadataFields = SCHEMA_DEFINITIONS as SchemaDefinitions;
 
@@ -27,51 +39,74 @@ const TopicCategories: React.FC<TopicCategoryProps> = ({
       )
       .sort() || [];
 
-  if (topicCategoryNames?.length === 0) return null;
-  return (
-    <Flex
-      flexWrap='wrap'
-      my={0}
-      py={1}
-      borderBottom='1px solid'
-      borderBottomColor='gray.200'
-      {...props}
-    >
-      <Tooltip label={metadataFields['topicCategory'].description?.[type]}>
-        <Text fontSize='xs' color='gray.800' mr={1} userSelect='none'>
-          Topic Categories
-          <Icon
-            as={FaInfo}
-            boxSize={3.5}
-            border='1px solid'
-            borderRadius='full'
-            p={0.5}
-            mx={1}
-            color='gray.800!important'
-          />
-          :
-        </Text>
-      </Tooltip>
+  const [limit, setLimit] = useState(DEFAULT_LIMIT);
 
-      {topicCategoryNames?.map(name => {
-        return (
-          <TagWithUrl
-            key={name}
-            colorScheme='primary'
-            href={{
-              pathname: '/search',
-              query: {
-                q: `topicCategory.name:"${name.trim()}"`,
-              },
-            }}
-            m={0.5}
-            leftIcon={FaMagnifyingGlass}
+  const toggleLimit = () => {
+    setLimit(prev =>
+      prev === topicCategoryNames.length
+        ? DEFAULT_LIMIT
+        : topicCategoryNames.length,
+    );
+  };
+
+  if (topicCategoryNames?.length === 0) return null;
+
+  return (
+    <ScrollContainer maxHeight='320px' m={0} p={0}>
+      <Flex
+        flexWrap='wrap'
+        my={0}
+        py={1}
+        borderBottom='1px solid'
+        borderBottomColor='gray.200'
+        {...props}
+      >
+        <Tooltip label={metadataFields['topicCategory'].description?.[type]}>
+          <Text fontSize='xs' color='gray.800' mr={1} userSelect='none'>
+            Topic Categories
+            <Icon
+              as={FaInfo}
+              boxSize={3.5}
+              border='1px solid'
+              borderRadius='full'
+              p={0.5}
+              mx={1}
+              color='gray.800!important'
+            />
+            :
+          </Text>
+        </Tooltip>
+        {topicCategoryNames?.slice(0, limit).map(name => {
+          return (
+            <TagWithUrl
+              key={name}
+              colorScheme='primary'
+              href={{
+                pathname: '/search',
+                query: {
+                  q: `topicCategory.name:"${name.trim()}"`,
+                },
+              }}
+              m={0.5}
+              leftIcon={FaMagnifyingGlass}
+            >
+              {name}
+            </TagWithUrl>
+          );
+        })}
+        {topicCategoryNames?.length > DEFAULT_LIMIT && (
+          <Button
+            size='xs'
+            variant='link'
+            justifyContent='flex-end'
+            m={1}
+            onClick={toggleLimit}
           >
-            {name}
-          </TagWithUrl>
-        );
-      })}
-    </Flex>
+            {generateButtonLabel(limit, topicCategoryNames.length)}
+          </Button>
+        )}
+      </Flex>
+    </ScrollContainer>
   );
 };
 
