ca1330dc882a633cc2e7c5211fed4286aedc36cd
candicecz <czech.candice@gmail.com>
2025-01-28 14:19:28 -0500

    chore: remove use_metadata_score flag

--- src/views/search-results-page/index.tsx
+++ src/views/search-results-page/index.tsx
@@ -137,7 +137,7 @@ const SearchResultsPage = ({
       size: `${selectedPerPage}`,
       from: `${(selectedPage - 1) * selectedPerPage}`,
       sort: sortOrder,
-      use_metadata_score: shouldUseMetadataScore ? 'true' : 'false',
+      // use_metadata_score: shouldUseMetadataScore ? 'true' : 'false',
       show_meta: true,
       fields: [
         '_meta',
