f8d4286fedc2fad26bf7bf336a56150b086e3493
candicecz <czech.candice@gmail.com>
2025-03-27 10:03:06 -0400

    feat(topic): add legend and links to donut chart

--- src/views/topics/components/data-types.tsx
+++ src/views/topics/components/data-types.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Flex, Text } from '@chakra-ui/react';
+import NextLink from 'next/link';
 import { SectionTitle } from '../layouts/section';
 import { Link } from 'src/components/link';
 import { TopicPageProps } from '../types';
@@ -12,16 +13,41 @@ import {
   APIResourceType,
   formatResourceTypeForDisplay,
 } from 'src/utils/formatting/formatResourceType';
+import { LegendContainer, LegendItem } from './legend';
+import { queryFilterObject2String } from 'src/views/search-results-page/helpers';
+import { UrlObject } from 'url';
 
 interface DataTypesProps {
   query: TopicPageProps['attributes']['query'];
   topic: string;
 }
 
+// Color scale for data types.
 const getFillColor = scaleOrdinal({
   domain: ['Dataset', 'ComputationalTool', 'ResourceCatalog'],
   range: ['#e8c543', '#ff8359', '#6e95fc'],
 });
+
+// Helper function to generate a URL object for search results.
+const getSearchResultsRoute = ({
+  querystring,
+  facet,
+  term,
+}: {
+  facet: string;
+  querystring: string;
+  term: string;
+}): UrlObject => {
+  return {
+    pathname: `/search`,
+    query: {
+      q: querystring,
+      filters: queryFilterObject2String({
+        [facet]: [term],
+      }),
+    },
+  };
+};
 
 export const DataTypes = ({ query, topic }: DataTypesProps) => {
   // Fetch data types for query.
@@ -46,30 +72,65 @@ export const DataTypes = ({ query, topic }: DataTypesProps) => {
   });
 
   return (
-    <Box maxWidth='50%'>
-      <SectionTitle as='h4'>Data Types</SectionTitle>
-      <Text lineHeight='short'>
-        An overview of resource types retrieved from a search on{' '}
-        <Link href={`/search?q=${query.q}`}>{topic}</Link>.
-      </Text>
+    <Flex>
+      <Box flex={3}>
+        <SectionTitle as='h4'>Data Types</SectionTitle>
+        <Text lineHeight='short'>
+          An overview of resource types retrieved from a search on{' '}
+          <Link href={`/search?q=${query.q}`}>{topic}</Link>.
+        </Text>
 
-      {/* Donut Chart */}
-      {data && (
-        <DonutChart
-          width={200}
-          height={200}
-          donutThickness={20}
-          data={data}
-          getFillColor={getFillColor}
-          labelStyles={{
-            fill: '#2f2f2f',
-            transformLabel: term =>
-              formatResourceTypeForDisplay(term as APIResourceType),
-          }}
-        />
-      )}
-      {/* TO DO: Add Error state */}
-      {/* TO DO: Add Loading state */}
-    </Box>
+        {/* Donut Chart */}
+        {data && (
+          <DonutChart
+            width={200}
+            height={200}
+            donutThickness={20}
+            data={data}
+            getFillColor={getFillColor}
+            labelStyles={{
+              fill: '#2f2f2f',
+              transformLabel: term =>
+                formatResourceTypeForDisplay(term as APIResourceType),
+            }}
+            getRoute={term => {
+              return getSearchResultsRoute({
+                facet: params.facets,
+                querystring: query.q,
+                term,
+              });
+            }}
+          />
+        )}
+        {/* TO DO: Add Error state */}
+        {/* TO DO: Add Loading state */}
+      </Box>
+      <Box flex={1}>
+        <LegendContainer>
+          {data?.map(({ term, count }) => {
+            return (
+              <LegendItem
+                key={term}
+                count={count}
+                swatchBg={getFillColor(term)}
+              >
+                <NextLink
+                  href={getSearchResultsRoute({
+                    facet: params.facets,
+                    querystring: query.q,
+                    term: term as string,
+                  })}
+                  passHref
+                >
+                  <Link>
+                    {formatResourceTypeForDisplay(term as APIResourceType)}
+                  </Link>
+                </NextLink>
+              </LegendItem>
+            );
+          })}
+        </LegendContainer>
+      </Box>
+    </Flex>
   );
 };
