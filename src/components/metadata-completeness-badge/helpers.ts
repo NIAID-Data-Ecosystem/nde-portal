695dd228cc43db757c3cf82106dff453c6dbc547
candicecz <czech.candice@gmail.com>
2025-04-16 15:07:32 -0400

    feat: retrieve metadata completeness fields from gh

--- src/components/metadata-completeness-badge/helpers.ts
+++ src/components/metadata-completeness-badge/helpers.ts
@@ -1,83 +1,8 @@
 import { FormattedResource } from 'src/utils/api/types';
+import METADATA_COMPLETENESS_FIELDS from 'src/components/metadata-completeness-badge/fields.json';
 
-const REQUIRED_FIELDS = [
-  'author',
-  'date',
-  'description',
-  'distribution',
-  'funding',
-  'includedInDataCatalog',
-  'measurementTechnique',
-  'name',
-  'url',
-];
-
-const RECOMMENDED_FIELDS = [
-  'citation',
-  'citedBy',
-  'conditionsOfAccess',
-  'dateCreated',
-  'dateModified',
-  'datePublished',
-  'doi',
-  'healthCondition',
-  'identifier',
-  'infectiousAgent',
-  'interactionStatistic',
-  'isBasedOn',
-  'keywords',
-  'license',
-  'sdPublisher',
-  'spatialCoverage',
-  'species',
-  'temporalCoverage',
-  'topicCategory',
-  'usageInfo',
-  'variableMeasured',
-];
-
+// Use the same list of required and recommended fields
+// https://github.com/NIAID-Data-Ecosystem/nde-crawlers/blob/main/biothings-hub/files/nde-hub/scores.py
 export const getMetadataListByType = (type: FormattedResource['@type']) => {
-  if (type === 'ResourceCatalog') {
-    const required = REQUIRED_FIELDS.concat(['collectionType']);
-    const recommended = RECOMMENDED_FIELDS.concat([
-      'collectionSize',
-      'hasAPI',
-      'hasDownload',
-    ]);
-    return { required, recommended };
-  } else if (type === 'ComputationalTool') {
-    const required = [
-      'date',
-      'includedInDataCatalog',
-      'funding',
-      'author',
-      'description',
-      'name',
-    ];
-    const recommended = [
-      'citedBy',
-      'doi',
-      'topicCategory',
-      'codeRepository',
-      'programmingLanguage',
-      'applicationCategory',
-      'applicationSubCategory',
-      'input',
-      'output',
-      'featureList',
-      'operatingSystem',
-      'softwareRequirements',
-      'softwareVersion',
-      'citation',
-      'conditionsOfAccess',
-      'dateModified',
-      'interactionStatistic',
-      'license',
-      'identifier',
-      'url',
-    ];
-    return { required, recommended };
-  }
-
-  return { required: REQUIRED_FIELDS, recommended: RECOMMENDED_FIELDS };
+  return METADATA_COMPLETENESS_FIELDS[type] || [];
 };
