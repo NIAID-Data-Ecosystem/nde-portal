1a27e561a8c0e7cf102f79354fee57da740906b9
candicecz <czech.candice@gmail.com>
2025-05-27 14:43:44 -0400

    refactor(search): add usemetadatascore toggle to search actions toolbar

--- src/views/draft-search/components/results/components/toolbar/index.tsx
+++ src/views/draft-search/components/results/components/toolbar/index.tsx
@@ -11,6 +11,7 @@ import {
   SORT_OPTIONS,
 } from 'src/views/draft-search/config/defaultQuery';
 import { Flex, Stack } from '@chakra-ui/react';
+import { MetadataScoreToggle } from './components/metadata-score-toggle';
 
 /*
 [COMPONENT INFO]:
@@ -31,56 +32,73 @@ export const SearchResultsToolbar = ({
   const { size, sort } = getPagination(id);
 
   return (
-    <Flex
-      borderBottom={{ base: '1px solid' }}
-      borderColor={{ base: 'page.alt' }}
-      flexDirection={{ base: 'column-reverse', md: 'row' }}
-      alignItems={{ base: 'unset', md: 'center' }}
-      justifyContent={'space-between'}
-      pb={2}
-      w='100%'
-    >
-      <Stack flexDirection={{ base: 'column', sm: 'row' }} spacing={[1, 4]}>
-        {/* Sort menu */}
-        <SelectWithLabel
-          id='sort-results'
-          label='Sort search results by:'
-          options={SORT_OPTIONS}
-          value={sort}
-          handleChange={newSort => {
-            const update = { sort: newSort, from: 1 };
-            setPagination(id, update);
-            updateRoute(router, update);
-          }}
-          minWidth='200px'
-        />
-
-        {/* Size menu */}
-        <SelectWithLabel
-          id='size-results'
-          label='Show number of results per page:'
-          options={PAGE_SIZE_OPTIONS}
-          value={size}
-          handleChange={newSize => {
-            const update = { size: +newSize, from: 1 };
-            // Update pagination state for the current tab.
-            setPagination(id, update);
-            updateRoute(router, update);
-          }}
-        />
-      </Stack>
-
-      {/* Download CTA */}
-      <DownloadMetadata
-        pb={{ base: 2, md: 0 }}
-        exportFileName={`nde-results-${(params.q ?? '').replaceAll(' ', '_')}`}
-        params={params}
-        buttonProps={{ variant: 'outline' }}
-      >
-        Download Metadata
-      </DownloadMetadata>
-
+    <>
       {/* Apply metadata score (optional) */}
-    </Flex>
+      {process.env.NEXT_PUBLIC_APP_ENV !== 'production' && (
+        <MetadataScoreToggle
+          isChecked={params.use_metadata_score === 'true'}
+          isDisabled={params.sort !== '_score'}
+          handleToggle={() => {
+            const newUseMetadataScore =
+              params.use_metadata_score === 'true' ? 'false' : 'true';
+            const update = { use_metadata_score: newUseMetadataScore, from: 1 };
+            setPagination(id, update);
+            updateRoute(router, update);
+          }}
+        />
+      )}
+      <Flex
+        borderBottom={{ base: '1px solid' }}
+        borderColor={{ base: 'page.alt' }}
+        flexDirection={{ base: 'column-reverse', md: 'row' }}
+        alignItems={{ base: 'unset', md: 'center' }}
+        justifyContent={'space-between'}
+        pb={2}
+        w='100%'
+      >
+        <Stack flexDirection={{ base: 'column', sm: 'row' }} spacing={[1, 4]}>
+          {/* Sort menu */}
+          <SelectWithLabel
+            id='sort-results'
+            label='Sort search results by:'
+            options={SORT_OPTIONS}
+            value={sort}
+            handleChange={newSort => {
+              const update = { sort: newSort, from: 1 };
+              setPagination(id, update);
+              updateRoute(router, update);
+            }}
+            minWidth='200px'
+          />
+
+          {/* Size menu */}
+          <SelectWithLabel
+            id='size-results'
+            label='Show number of results per page:'
+            options={PAGE_SIZE_OPTIONS}
+            value={size}
+            handleChange={newSize => {
+              const update = { size: +newSize, from: 1 };
+              // Update pagination state for the current tab.
+              setPagination(id, update);
+              updateRoute(router, update);
+            }}
+          />
+        </Stack>
+
+        {/* Download CTA */}
+        <DownloadMetadata
+          pb={{ base: 2, md: 0 }}
+          exportFileName={`nde-results-${(params.q ?? '').replaceAll(
+            ' ',
+            '_',
+          )}`}
+          params={params}
+          buttonProps={{ variant: 'outline' }}
+        >
+          Download Metadata
+        </DownloadMetadata>
+      </Flex>
+    </>
   );
 };
