3a314aa42cd7887d8ca5a5c540fd0002f037fff7
candicecz <czech.candice@gmail.com>
2025-05-29 14:47:44 -0400

    chore: remove draft-search from prod view

--- src/pages/draft-search.tsx
+++ src/pages/draft-search.tsx
@@ -82,6 +82,13 @@ const Search: NextPage<{
     setInitialTab(tab?.id || defaultTab);
   }, [router]);
 
+  // If the app is in production, redirect to a 404 page until search is fully implemented and approved.
+  useEffect(() => {
+    if (process.env.NEXT_PUBLIC_APP_ENV === 'production') {
+      router.replace('/404');
+    }
+  }, [router]);
+
   // If the initial tab is not set, return a loading state.
   if (!initialTab) {
     return null;
@@ -149,15 +156,6 @@ const Search: NextPage<{
 };
 
 export async function getStaticProps() {
-  // If the app is in production, redirect to a 404 page until search is fully implemented and approved.
-  if (process.env.NEXT_PUBLIC_APP_ENV === 'production') {
-    return {
-      redirect: {
-        destination: '/404',
-        permanent: false,
-      },
-    };
-  }
   try {
     const defaultParams = {
       q: defaultQuery.q,
