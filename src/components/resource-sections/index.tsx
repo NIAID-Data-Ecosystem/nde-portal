9153a13cc32e83741722b519d9f43ef0e23e3503
candicecz <czech.candice@gmail.com>
2025-06-06 13:07:04 -0400

    fix: remove unnecessary prop

--- src/components/resource-sections/index.tsx
+++ src/components/resource-sections/index.tsx
@@ -405,11 +405,7 @@ const Sections = ({
               )}
             {/* Show provenance */}
             {section.hash === 'provenance' && (
-              <ResourceProvenance
-                isLoading={isLoading}
-                type={data?.['@type']}
-                {...data}
-              />
+              <ResourceProvenance isLoading={isLoading} {...data} />
             )}
 
             {/* Show downloads */}
