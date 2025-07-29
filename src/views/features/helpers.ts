ac48b021ce6078a683119a3566bb99ad87ac04f8
candicecz <czech.candice@gmail.com>
2025-07-29 11:08:37 -0400

    fix(features): abstract length typo

--- src/views/features/helpers.ts
+++ src/views/features/helpers.ts
@@ -60,6 +60,8 @@ export const fetchAllFeaturedPages = async (params?: FeaturedQueryParams) => {
   }
 };
 
+const MAX_SHORT_DESCRIPTION_LENGTH = 160;
+
 export const transformFeaturedContentForCarousel = (
   data: FeaturedPageProps[],
 ): NewsOrEventsObject[] => {
@@ -70,7 +72,8 @@ export const transformFeaturedContentForCarousel = (
       name: item.title,
       type: 'feature',
       description: item.content,
-      shortDescription: item?.abstract.slice(0, 160) || '',
+      shortDescription:
+        item?.abstract?.slice(0, MAX_SHORT_DESCRIPTION_LENGTH) || '',
       image: item.thumbnail,
     };
   });
