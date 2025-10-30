945a88e0f67f9c4e784d305a926ac6c28624bca6
Leandro Collares <leco@uvic.ca>
2025-10-30 18:28:56 -0300

    fix: adjust partial bar's height

--- src/views/search/components/filters/components/date-slider/components/histogram.tsx
+++ src/views/search/components/filters/components/date-slider/components/histogram.tsx
@@ -258,7 +258,7 @@ const Histogram: React.FC<HistogramProps> = ({
                               : params.opacity.active
                           }
                           y={height - defaultBarHeight}
-                          height={barY}
+                          height={defaultBarHeight}
                           fill={params.fill.gray}
                         />
                       )}
