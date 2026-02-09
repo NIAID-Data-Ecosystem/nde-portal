c5872ca07d95ea6c1de0c5cab806e4aaed2d48cf
candicecz <czech.candice@gmail.com>
2026-02-09 15:47:06 -0500

    style: modify navbar z-index

--- src/components/navigation-bar/index.tsx
+++ src/components/navigation-bar/index.tsx
@@ -47,7 +47,7 @@ export const Navigation: React.FC<FlexProps> = props => {
       as='nav'
       w='100%'
       minW={300}
-      zIndex='popover'
+      zIndex='modal'
       aria-label='Main navigation'
       {...props}
     >
