16b2f8119d5ced38845707771ec7a4aacbc5a8fb
candicecz <czech.candice@gmail.com>
2026-03-20 10:08:42 -0400

    fix(search): resource catalog carousel display

--- src/views/search/components/search-results-tabs-controller/index.tsx
+++ src/views/search/components/search-results-tabs-controller/index.tsx
@@ -69,7 +69,6 @@ export const SearchResultsController = ({
   };
 
   const queryParams = useSearchQueryFromURL();
-
   const searchResultsData = useSearchResultsData(
     {
       ...queryParams,
@@ -137,8 +136,10 @@ export const SearchResultsController = ({
       ...queryParams,
       q: queryParams.q,
       filters: { ...queryParams.filters, ['@type']: ['ResourceCatalog'] },
+      fields: CAROUSEL_RESULTS_FIELDS,
       facets: ['@type'],
-      facet_size: 100,
+      size: 50,
+      sort: 'name.raw',
       use_ai_search: queryParams.use_ai_search,
     },
 
