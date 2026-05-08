38a1348b457b6631645d58f5ad01a8834aca427f
candicecz <czech.candice@gmail.com>
2026-05-08 11:51:21 -0400

    chore: update source metadata types

--- src/hooks/api/types.ts
+++ src/hooks/api/types.ts
@@ -1,4 +1,11 @@
-import { Domain } from 'src/utils/api/types';
+import {
+  DefinedTerm,
+  Domain,
+  QuantitativeValue,
+  SpatialCoverage,
+  TemporalCoverage,
+  UsageInfo,
+} from 'src/utils/api/types';
 
 export interface MetadataSource {
   code: {
@@ -9,12 +16,38 @@ export interface MetadataSource {
     url: string;
   };
   sourceInfo: {
+    _id: string;
+    abstract?: string;
+    alternateName?: string[];
+    collectionSize?: QuantitativeValue[];
+    collectionType?: string;
+    conditionsOfAccess?: string;
+    date?: string;
+    dateModified?: string;
+    datePublished?: string;
+    description?: string;
+    genre?: (string | Domain)[];
+    hasAPI?: string;
+    hasDownload?: string;
+    healthCondition?: DefinedTerm[];
+    identifier?: string;
+    infectiousAgent?: DefinedTerm[];
+    inLanguage?: string;
+    isAccessibleForFree?: string;
+    license?: string;
+    measurementTechnique?: DefinedTerm[];
     name: string;
-    abstract: string;
-    description: string;
+    parentCollection?: { id: string };
+    schedule?: string;
     schema: Object | null;
-    url: string;
-    identifier: string;
+    spatialCoverage?: SpatialCoverage[];
+    species?: DefinedTerm[];
+    temporalCoverage?: TemporalCoverage[];
+    topicCategory?: DefinedTerm[];
+    type?: string;
+    url?: string;
+    usageInfo?: UsageInfo | string;
+    version?: string;
     metadata_completeness: {
       avg_augmented_recommended_ratio: number;
       avg_augmented_required_ratio: number;
@@ -65,10 +98,6 @@ export interface MetadataSource {
       percent_required_fields: number;
       percent_recommended_fields: number;
     };
-    conditionsOfAccess?: string;
-    genre?: Domain;
-    parentCollection?: { id: string };
-    type: string;
   };
   stats: { [key: string]: number };
   version: string;
