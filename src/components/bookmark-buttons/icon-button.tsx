afee673b7d57c84f226e7530c6400a8e12ca4106
candicecz <czech.candice@gmail.com>
2026-05-27 09:58:17 -0400

    feat: add saved search icon button

---
+++ src/components/bookmark-buttons/icon-button.tsx
@@ -0,0 +1,31 @@
+import { Icon, IconButton, IconButtonProps } from '@chakra-ui/react';
+import { FaBookmark, FaRegBookmark } from 'react-icons/fa6';
+
+export const BookmarkIconButton: React.FC<
+  Omit<IconButtonProps, 'aria-label'> & {
+    isFavorited: boolean;
+    'aria-label'?: IconButtonProps['aria-label'];
+  }
+> = ({ isFavorited, onClick, 'aria-label': ariaLabel, ...rest }) => {
+  return (
+    <IconButton
+      icon={
+        isFavorited ? (
+          <Icon as={FaBookmark} fill='link.color' w='inherit' />
+        ) : (
+          <Icon as={FaRegBookmark} fill='page.placeholder' w='inherit' />
+        )
+      }
+      aria-label={
+        ariaLabel ||
+        (isFavorited ? 'Remove bookmark for this query' : 'Bookmark this query')
+      }
+      onClick={onClick}
+      variant='ghost'
+      isRound={true}
+      borderRadius='50%'
+      size='sm'
+      {...rest}
+    />
+  );
+};
--- src/views/search/components/search-results-header/index.tsx
+++ src/views/search/components/search-results-header/index.tsx
@@ -1,6 +1,16 @@
-import { Flex, FlexProps, Text, TextProps, VStack } from '@chakra-ui/react';
+import {
+  Flex,
+  FlexProps,
+  HStack,
+  Text,
+  TextProps,
+  VStack,
+} from '@chakra-ui/react';
+import { BookmarkIconButton } from 'src/components/bookmark-buttons/icon-button';
 import { Link } from 'src/components/link';
 import { AI_ASSISTED_SEARCH_KC_LINK } from 'src/components/page-container/components/search/components/ai-toggle';
+import { useUserData } from 'src/hooks/useUserData';
+import { ENABLE_AUTH } from 'src/utils/feature-flags';
 
 export const SearchResultsHeading = ({ children, ...props }: TextProps) => {
   return (
@@ -39,6 +49,14 @@ export const SearchResultsHeader = ({
   querystring: string;
   showAIBanner: boolean | null;
 }) => {
+  const { favoriteSearches, saveFavoriteSearch, removeFavoriteSearch } =
+    useUserData();
+
+  const favoriteIndex = favoriteSearches.findIndex(
+    search => search.query === querystring,
+  );
+  const isFavorited = favoriteIndex !== -1;
+
   return (
     <VStack alignItems='flex-start' spacing={1} fontSize='sm' flex={1}>
       {showAIBanner && (
@@ -66,9 +84,25 @@ export const SearchResultsHeader = ({
       </SearchResultsHeading>
       {/* Query string */}
       {querystring !== '__all__' && (
-        <Text color='text.heading' fontSize='inherit' fontWeight='medium'>
-          {querystring.replaceAll('\\', '')}
-        </Text>
+        <HStack spacing={1} width='100%' alignItems='flex-start'>
+          <Text color='text.heading' fontSize='inherit' fontWeight='medium'>
+            {querystring.replaceAll('\\', '')}
+          </Text>
+
+          {ENABLE_AUTH && (
+            <BookmarkIconButton
+              isFavorited={isFavorited}
+              onClick={() =>
+                isFavorited
+                  ? removeFavoriteSearch(favoriteIndex)
+                  : saveFavoriteSearch({
+                      query: querystring,
+                      name: `Search: ${querystring}`,
+                    })
+              }
+            />
+          )}
+        </HStack>
       )}
     </VStack>
   );
