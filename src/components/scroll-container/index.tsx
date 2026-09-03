5e572ddc35765156f30bb3ec4e2282b737d96969
candicecz <czech.candice@gmail.com>
2026-09-03 15:05:40 -0400

    chore: use chakra scroll area in scroll container

--- src/components/scroll-container/index.tsx
+++ src/components/scroll-container/index.tsx
@@ -1,35 +1,44 @@
-import { Box } from '@chakra-ui/react';
+import { ScrollArea } from '@chakra-ui/react';
 
-export const ScrollContainer = ({ children, ...props }: any) => {
+export interface ScrollContainerProps
+  extends React.ComponentProps<typeof ScrollArea.Root> {
+  showScrollShadow?: boolean;
+}
+
+export const ScrollContainer = ({
+  children,
+  showScrollShadow,
+  variant = 'always',
+  ...props
+}: ScrollContainerProps) => {
   return (
-    <Box
-      overflow='auto'
-      pr={2}
-      css={{
-        '& &::-webkit-scrollbar': {
-          width: '8px',
-          height: '7px',
-        },
-
-        '& &::-webkit-scrollbar-track': {
-          background: 'blackAlpha.100',
-          borderRadius: '8px',
-        },
-
-        '& &::-webkit-scrollbar-thumb': {
-          background: 'gray.300',
-          borderRadius: '8px',
-        },
-
-        '& _hover': {
-          '&::-webkit-scrollbar-thumb': {
-            background: 'text.placeholder',
-          },
-        },
-      }}
-      {...props}
-    >
-      {children}
-    </Box>
+    <ScrollArea.Root variant={variant} {...props}>
+      <ScrollArea.Viewport
+        css={
+          showScrollShadow && {
+            '--scroll-shadow-size': '4rem',
+            maskImage: 'linear-gradient(#000, #000)',
+            '&[data-overflow-y]': {
+              maskImage:
+                'linear-gradient(#000,#000,transparent 0,#000 var(--scroll-shadow-size),#000 calc(100% - var(--scroll-shadow-size)),transparent)',
+              '&[data-at-top]': {
+                maskImage:
+                  'linear-gradient(180deg,#000 calc(100% - var(--scroll-shadow-size)),transparent)',
+              },
+              '&[data-at-bottom]': {
+                maskImage:
+                  'linear-gradient(0deg,#000 calc(100% - var(--scroll-shadow-size)),transparent)',
+              },
+            },
+          }
+        }
+      >
+        <ScrollArea.Content>{children}</ScrollArea.Content>
+      </ScrollArea.Viewport>
+      <ScrollArea.Scrollbar>
+        <ScrollArea.Thumb />
+      </ScrollArea.Scrollbar>
+      <ScrollArea.Corner />
+    </ScrollArea.Root>
   );
 };
